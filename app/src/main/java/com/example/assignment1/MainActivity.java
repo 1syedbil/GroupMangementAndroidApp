e6743bfeefcf2d6576ae8b3523740ce9271321e5
Bilal Syed <bsyed7633@conestogac.on.ca>
2025-02-13 00:16:15 -0500

    Created the home page UI in the main activity and added functionality to the "Manager" button so that it shows the manager login UI in the main activity.

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.assignment1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.activity.ComponentActivity;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends ComponentActivity {
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        Button managerBtn = findViewById(R.id.managerButton);
+        Button crewBtn = findViewById(R.id.crewButton);
+        TextView roleLabel = findViewById(R.id.roleLabel);
+
+        managerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showManagerLogin(managerBtn, crewBtn, roleLabel);
+            }
+        });
     }
+
+    private void showManagerLogin(Button managerBtn, Button crewBtn, TextView roleLabel){
+        ManagerLogin login = new ManagerLogin();
+
+        roleLabel.setVisibility(View.GONE);
+        managerBtn.setVisibility(View.GONE);
+        crewBtn.setVisibility(View.GONE);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.managerLogin, login).commit();
+    }
+
 }
---
+++ app/src/main/java/com/example/assignment1/ManagerLogin.java
@@ -0,0 +1,23 @@
+package com.example.assignment1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ManagerLogin extends Fragment {
+
+    public ManagerLogin(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_manager_login, container, false);
+    }
+}
