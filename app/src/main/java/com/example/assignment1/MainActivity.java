58fc6740f2fb5933d02961c062a333754c8c045c
Bilal Syed <bsyed7633@conestogac.on.ca>
2025-02-13 15:12:57 -0500

    Added a back button on the manager login screen to go back to the role selection screen. Added functionality to the login button on the manager login screen so that when the button is pressed, the application goes to activity 2.

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -12,31 +12,59 @@ import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ManagerLogin login;
+    private Button managerBtn;
+    private Button crewBtn;
+    private Button backBtn;
+    private TextView roleLabel;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        Button managerBtn = findViewById(R.id.managerButton);
-        Button crewBtn = findViewById(R.id.crewButton);
-        TextView roleLabel = findViewById(R.id.roleLabel);
+        managerBtn = findViewById(R.id.managerButton);
+        crewBtn = findViewById(R.id.crewButton);
+        roleLabel = findViewById(R.id.roleLabel);
+        backBtn = findViewById(R.id.backButton);
+
+        backBtn.setVisibility(View.GONE);
 
         managerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showManagerLogin(managerBtn, crewBtn, roleLabel);
+                login = showManagerLogin(managerBtn, crewBtn, backBtn, roleLabel);
             }
         });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                getSupportFragmentManager().beginTransaction().remove(login).commit();
+
+                roleLabel.setVisibility(View.VISIBLE);
+                managerBtn.setVisibility(View.VISIBLE);
+                crewBtn.setVisibility(View.VISIBLE);
+                backBtn.setVisibility(View.GONE);
+            }
+        });
+
     }
 
-    private void showManagerLogin(Button managerBtn, Button crewBtn, TextView roleLabel){
+    private ManagerLogin showManagerLogin(Button managerBtn, Button crewBtn, Button backBtn, TextView roleLabel){
+
         ManagerLogin login = new ManagerLogin();
 
         roleLabel.setVisibility(View.GONE);
         managerBtn.setVisibility(View.GONE);
         crewBtn.setVisibility(View.GONE);
+        backBtn.setVisibility(View.VISIBLE);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.managerLogin, login).commit();
+
+        return login;
+
     }
 
 }
--- app/src/main/java/com/example/assignment1/ManagerLogin.java
+++ app/src/main/java/com/example/assignment1/ManagerLogin.java
@@ -1,23 +1,43 @@
 package com.example.assignment1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class ManagerLogin extends Fragment {
 
-    public ManagerLogin(){
+    public ManagerLogin(){}
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_manager_login, container, false);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_manager_login, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button loginButton = view.findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V) {
+                Intent intent = new Intent(getActivity(), SecondActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
+
 }
